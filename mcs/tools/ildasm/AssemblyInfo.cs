b943432330abd2eecd80b31ee50cadc1694adbe7
XTZGZoReX <xtzgzorex@gmail.com>
2011-08-01 20:58:54 +0200

    ildasm: Project skeleton.

---
+++ mcs/tools/ildasm/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+
+[assembly: AssemblyVersion (Consts.MonoVersion)]
+[assembly: AssemblyTitle ("Mono IL Disassembler")]
+[assembly: AssemblyDescription ("Mono ILAsm Language Disassembler")]
+[assembly: AssemblyCopyright ("Alex Rønne Petersen, Jb Evain")]
